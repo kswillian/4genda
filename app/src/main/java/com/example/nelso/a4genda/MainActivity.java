1230212467cdddda7f1dcdf2003b27fb9194f781
Camila <camila.koga@timo.com.br>
2019-06-19 02:22:45 -0300

    Ajustes Splash Screen e criação da telas Main e Novo Contato

--- app/src/main/java/com/example/nelso/a4genda/MainActivity.java
+++ app/src/main/java/com/example/nelso/a4genda/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.nelso.a4genda;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewContactActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/nelso/a4genda/NewContactActivity.java
@@ -0,0 +1,14 @@
+package com.example.nelso.a4genda;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_contact);
+    }
+}
